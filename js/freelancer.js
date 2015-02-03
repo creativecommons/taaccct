8ee54a62cc9b3c333e367d5ff2796c6ca74c355d
Justin Hedani <jkhedani@gmail.com>
2015-02-02 22:21:56 -1000

    Patch last issues.

--- js/freelancer.js
+++ js/freelancer.js
@@ -72,6 +72,27 @@ jQuery( document ).ready(function( $ ) {
   $('.portfolio-link').on('click', function() {
     $(this).addClass('current');
   });
+
+  // Smooth Scroll
+  var hashTagActive = "";
+  $(".smooth-anchor-scroll").click(function (event) {
+    if(hashTagActive != this.hash) { //this will prevent if the user click several times the same link to freeze the scroll.
+      event.preventDefault();
+      //calculate destination place
+      var dest = 0;
+      if ($(this.hash).offset().top > $(document).height() - $(window).height()) {
+          dest = $(document).height() - $(window).height();
+      } else {
+          dest = $(this.hash).offset().top;
+      }
+      //go to destination
+      $('html,body').animate({
+          scrollTop: dest
+      }, 1200, 'swing');
+      hashTagActive = this.hash;
+  }
+  });
+
 }); // jQuery
 
 // When prev or next is clicked, close current modal...
